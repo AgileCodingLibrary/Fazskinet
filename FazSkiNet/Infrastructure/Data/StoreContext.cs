5f7a417b168d374586083fff906b53ab6db2dcea
faz ahmed <fazahmed20xx@hotmail.com>
2020-07-26 12:05:42 +0100

    Added DbContext class in the Infrastructure Data folder.

---
+++ FazSkiNet/Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,19 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductBrand> ProductBrands { get; set; }
+        public DbSet<ProductType> ProductTypes { get; set; }
+    }
+}
